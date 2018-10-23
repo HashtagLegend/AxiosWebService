17b45613df7cdef61f2ee6ce896f71ca9729a2b0
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-10-23 22:00:16 +0200

    created axios get function

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,48 @@
-function performGetRequest1() {
-    var resultElement = document.getElementById('getResult1');
-    resultElement.innerHTML = '';
-    
-    axios.get('http://jsonplaceholder.typicode.com/todos')
-      .then(function (response) {
-        resultElement.innerHTML = generateSuccessHTMLOutput(response);
+import axios, {AxiosResponse, AxiosError} from "../../node_modules/axios/index";
+
+interface ICustomer{
+  id: number;
+  firstName: string;
+  lastName: string;
+  year: number;
+}
+
+let outputElement: HTMLParagraphElement = document.getElementById("outputField") as HTMLParagraphElement
+
+let getButton: HTMLButtonElement = document.getElementById("get") as HTMLButtonElement
+getButton.addEventListener("click", getCustomers)
+
+/*
+function get()
+{
+  let uri: string = "https://restcustomers3rvice.azurewebsites.net/api/customer"
+  axios.get(uri)
+    .then(function (AxiosResponse)
+    {
+      console.log(AxiosResponse)
+    });  
+}
+*/
+
+function getCustomers(): void {
+
+  let uri: string = "https://restcustomers3rvice.azurewebsites.net/api/customer";
+
+  axios.get<ICustomer[]>(uri)
+
+      .then(function (response: AxiosResponse<ICustomer[]>): void {
+          let result: string = "<ol>";
+
+          response.data.forEach((c: ICustomer) => {
+
+              result += "<li>" + c.firstName + " " + c.lastName + "</li>";
+
+          });
+
+          result += "</ol>";
+
+          outputElement.innerHTML = result;
+
       })
-      .catch(function (error) {
-        resultElement.innerHTML = generateErrorHTMLOutput(error);
-      });   
-  }
+
+
